8aea77b5d3b47714d7736311ee33f43738cfe4ba
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-02 11:32:21 +0200

    variables ok

--- 003-variables/main.go
+++ 003-variables/main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+const path = "usr/bin"
+
 func separator() {
 	fmt.Println("---")
 }
@@ -10,6 +12,7 @@ func main() {
 	var message string = "greetings"
 	var age = 42
 	var answer int
+	rank := 5.4 // same as with var, but only inside functions
 
 	fmt.Printf("The message is \"%s\".\n", message)
 	fmt.Printf("The message type is \"%T\".\n", message)
@@ -19,4 +22,11 @@ func main() {
 	separator()
 	fmt.Printf("The answer is %d.\n", answer)
 	fmt.Printf("The answer type is \"%T\".\n", answer)
+	separator()
+	fmt.Printf("The rank is %f.\n", rank)
+	fmt.Printf("The rank is %.2f.\n", rank)
+	fmt.Printf("The rank type is \"%T\".\n", rank)
+	separator()
+	fmt.Printf("The constant path is \"%s\".\n", path)
+	fmt.Printf("The constant path type is \"%T\".\n", path)
 }
