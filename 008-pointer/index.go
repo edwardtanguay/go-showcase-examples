f6711059ccb572b93c37420e224809eaa0f1d471
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-02 15:36:02 +0200

    00882

--- 008-pointer/index.go
+++ 008-pointer/index.go
@@ -2,18 +2,19 @@ package main
 
 import "fmt"
 
+func printValues(i int, p *int) {
+	fmt.Printf("The age is %d\n", i)
+	fmt.Printf("The pointer is %d\n", p)
+	fmt.Printf("The pointer points to the value %d\n", *p)
+}
+
 func main() {
 	age := 34
 	pAge := &age
 
-	fmt.Printf("The age is %d\n", age)
-	fmt.Printf("The pointer is %d\n", pAge)
-	fmt.Printf("The pointer points to the value %d\n", *pAge)
-
+	printValues(age, pAge)
 	*pAge += 1
 
 	fmt.Println("--- VALUE UNDER POINTER CHANGED")
-	fmt.Printf("The age is %d\n", age)
-	fmt.Printf("The pointer is %d\n", pAge)
-	fmt.Printf("The pointer points to the value %d\n", *pAge)
+	printValues(age, pAge)
 }
