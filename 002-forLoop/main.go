ee192ae9125cd28ba2bf06bf714dd955f747aaec
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-02 09:32:24 +0200

    Printf and len()

---
+++ 002-forLoop/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+
+	word := "dictionary"
+
+	fmt.Printf("The sum is %d\n", sum)
+	fmt.Printf("The length of the word \"%s\" is %d\n", word, len(word))
+}
