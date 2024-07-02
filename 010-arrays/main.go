f72930fd59d2600feca6764f47bfea80b7bb6281
Edward Tanguay <edward@tanguay.info>
2024-07-02 16:42:24 +0200

    arrays

--- 010-arrays/main.go
+++ 010-arrays/main.go
@@ -1,8 +1,18 @@
-
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Println("nnn")
+
+	// multiline defintion
+	var colors [3]string
+	colors[0] = "red"
+	colors[1] = "blue"
+	colors[2] = "green"
+	fmt.Printf("The second color is %s.\n", colors[1])
+
+	// single line defintion
+	scores := [4]int{89, 75, 99, 66}
+	fmt.Printf("The last score is %d.\n", scores[len(scores)-1])
+
 }
