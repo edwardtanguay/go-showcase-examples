d1ea49cc96430205471cc55d947a7fb95a8b5bcd
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-03 11:53:49 +0200

    fetch html finished

--- 020-fetch/main.go
+++ 020-fetch/main.go
@@ -13,27 +13,33 @@ func main() {
 	resp, err := http.Get(url)
 	checkError(err)
 
-	fmt.Printf("Response type: %T\n", resp)
 	defer resp.Body.Close()
 
 	bytes, err := io.ReadAll(resp.Body)
 	checkError(err)
 
 	json := string(bytes)
-	fmt.Print(json)
 
-	skills := getSkillsFromJson(json)
-
-	for i, skill := range skills {
-		fmt.Printf("Skill %d:\n", i+1)
-		fmt.Printf("  ID Code: %s\n", skill.IdCode)
-		fmt.Printf("  Name: %s\n", skill.Name)
-		fmt.Printf("  URL: %s\n", skill.Url)
-		fmt.Printf("  Description: %s\n\n", skill.Description)
-	}
+	skills := convertJsonToSkills(json)
+	htmlContent := convertSkillsToHtml(skills)
+	createTextFile("skills.html", htmlContent)
 }
 
-func getSkillsFromJson(content string) []Skill {
+func convertSkillsToHtml(skills []Skill) string {
+	html := ""
+	title := "Skills"
+	html += getHtmlBoilerplateBegin(title)
+	html += fmt.Sprintf("<h1>%s</h1>", title)
+	for _, skill := range skills {
+		html += fmt.Sprintf("<h2><a target=\"_blank\" href=\"%s\">%s</a></h2>\n", skill.Url, skill.Name)
+		html += fmt.Sprintf("<p>%s</p>\n", skill.Description)
+		html += fmt.Sprintf("\n")
+	}
+	html += getHtmlBoilerplateEnd()
+	return html
+}
+
+func convertJsonToSkills(content string) []Skill {
 	var skills []Skill
 	err := json.Unmarshal([]byte(content), &skills)
 	checkError(err)
--- 020-fetch/tools.go
+++ 020-fetch/tools.go
@@ -28,3 +28,45 @@ func createTextFile(fileName, content string) {
 	checkError(err)
 	defer file.Close()
 }
+
+func getHtmlBoilerplateBegin(title string) string {
+	return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>` + title + `</title>
+	<style>
+		* {
+			margin: 0;
+			padding: 0;
+			box-sizing: border-box;
+		}
+		body {
+			font-family: monospace;
+  			line-height: 1.5;
+			padding: 1rem;
+		}
+		h2 {
+			font-size: 1.2rem;
+			margin-top: .3rem;
+		}
+		p {
+			font-size: 1rem;
+			font-style: italic;
+		}
+		a {
+			color: #333;
+		}
+	</style>
+</head>
+<body>
+`
+}
+
+func getHtmlBoilerplateEnd() string {
+	return `
+</body>
+</html>`
+}
