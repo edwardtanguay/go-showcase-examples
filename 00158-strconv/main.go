003f283983c65a1aeb1aea04bd2f0b944ecb6b75
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-16 03:36:52 +0200

    strings

---
+++ 00158-strconv/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	num := 100
+	str := string(num)
+	fmt.Printf("%v is of type %T\n", str, str) // not "100" but 'd' as rune
+
+	str2 := strconv.Itoa(num)
+	fmt.Printf("%v is of type %T\n", str2, str2)
+}
