143ce0467c4e4bb9775372daaaf086ceb8caa937
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-16 17:15:43 +0200

    directs

---
+++ 01929-files-getFiles/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func getFiles(dirPath string) ([]string, error) {
+	var fileList []string
+
+	err := filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() && strings.HasSuffix(info.Name(), ".json") {
+			fileList = append(fileList, path)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return fileList, nil
+}
+
+func main() {
+	dirPath := "." 
+	files, err := getFiles(dirPath)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("JSON Files:")
+	for _, file := range files {
+		fmt.Println(file)
+	}
+}
