fb70cdbedd651e93d410e705bd0be45759b3c62a
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-16 13:34:47 +0200

    filesize

--- 01910-files-os/main.go
+++ 01910-files-os/main.go
@@ -14,12 +14,25 @@ func fileExists(pathAndFileName string) bool {
 	return true
 }
 
+func getSizeAndModTime(pathAndFileName string) (int64, string) {
+	fileInfo, err := os.Stat(pathAndFileName)
+	if err != nil {
+		panic(err)
+	}
+	return fileInfo.Size(), fileInfo.ModTime().Format("2006-01-02 15:04:05")
+}
+
 func main() {
-	separator()
 
 	if(fileExists("mainx.go")) {
 		fmt.Println("yes")
 	} else {
 		fmt.Println("no")
 	}
+
+	size, when := getSizeAndModTime("main.go")
+	fmt.Printf("Size: %.2d KB, Modification Time: %s\n", size, when)
+
+
 }
+
