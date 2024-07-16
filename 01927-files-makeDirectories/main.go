5e0a1c2ff83c4cc6ab162a5de12bee1868c3beb4
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-16 15:47:23 +0200

    before delete

---
+++ 01927-files-makeDirectories/main.go
@@ -0,0 +1,24 @@
+
+package main
+
+import "os"
+
+func createNestedDirectories(directoryPath string) {
+	err := os.MkdirAll(directoryPath, os.ModePerm);
+	 if err != nil {
+		panic(err)
+	 }
+}
+
+func deleteNestedDirectories(directoryPath string) {
+	 err := os.RemoveAll(directoryPath)
+	 if err != nil {
+		panic(err)
+	 }
+}
+
+func main() {
+	// createNestedDirectories("output/images")	
+	// createNestedDirectories("output/data")	
+	deleteNestedDirectories("output")
+}
