cd8638c62a1baa0513e140813e439ba7d2bf7db0
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-17 00:31:22 +0200

    read json works

--- 02130-jsonDecode/main.go
+++ 02130-jsonDecode/main.go
@@ -1,14 +1,15 @@
-
 package main
 
-import "fmt"
-import "os"
-import "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
 
 type HtmlColor struct {
-	Hex string 			`json:"hex"`
-	HtmlName string 	`json:"name"`
-	Color string 		`json:"textColor"`
+	Hex      string `json:"hex"`
+	HtmlName string `json:"name"`
+	Color    string `json:"textColor"`
 }
 
 func main() {
@@ -19,4 +20,7 @@ func main() {
 	json.Unmarshal(content, &htmlColors)
 
 	fmt.Printf("%#v\n", htmlColors)
+	for i, htmlColor := range htmlColors {
+		fmt.Printf("%d. %s\n", i+1, htmlColor.HtmlName)
+	}
 }
