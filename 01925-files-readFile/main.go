9e8119ae175585913175bd67d2cfb0f6cfc1ba9e
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-16 14:38:08 +0200

    bad output

--- 01925-files-readFile/main.go
+++ 01925-files-readFile/main.go
@@ -6,16 +6,19 @@ import (
 	"strings"
 )
 
-func getLinesFromFile(fileName string) {
+func getLinesFromFile(fileName string) []string {
 	byteContents, err := os.ReadFile(fileName)
 	if err != nil {
 		panic(err)
 	}
 	contents := string(byteContents)
 	lines := strings.Split(contents, "\n")
-	fmt.Println(strings.Join(lines, "|"))
+	return lines
 }
 
 func main() {
-	getLinesFromFile("main.go")
+	lines := getLinesFromFile("main.go")
+	for i,line := range lines {
+		fmt.Printf("%d. %s\n", i+1, line)
+	}
 }
