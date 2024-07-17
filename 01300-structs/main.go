3be72d856a704d22651bb3538e23be5a551510ee
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-17 10:55:26 +0200

    notes

--- 01300-structs/main.go
+++ 01300-structs/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	emp := Employee{"Hans", "Hansrodt", 34}
+	emp := Employee{"Hans", "Hansrodt", 34, "an employee"}
 	separator()
 	fmt.Println(emp)
 	fmt.Printf("Employee: %v\n", emp)
@@ -13,10 +13,14 @@ func main() {
 	emp.Age = emp.Age + 10
 	separator()
 	fmt.Printf("The employee %v is %v years old.\n", emp.FirstName + " " + emp.LastName, emp.Age)
+	emp.notes = "the CEO"
+	println(emp.notes)
+	fmt.Printf("%#v", emp)
 }
 
 type Employee struct {
 	FirstName string
 	LastName string
 	Age int
+	notes string
 }
