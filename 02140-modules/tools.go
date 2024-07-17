c9ce50a478da869145d9b46c791836dd352f9ce3
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-17 09:17:50 +0200

    with modules

---
+++ 02140-modules/tools.go
@@ -0,0 +1,24 @@
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func separator(title ...string) {
+	width := 50
+	preWidth := 3
+	character := "="
+	if len(title) == 0 || title[0] == "" {
+		fmt.Println(strings.Repeat(character, width))
+	} else {
+		fmt.Printf("%s %s %s\n", strings.Repeat(character, preWidth), strings.ToUpper(title[0]), strings.Repeat(character, width - len(title[0]) - preWidth - 2))
+	}
+}
+
+func CheckError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
