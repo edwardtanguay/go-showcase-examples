bdb8f89c86d41054529c103dfdcfa92c3467f2de
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-19 11:18:32 +0200

    dog3

--- 01810-nakedReturn/main.go
+++ 01810-nakedReturn/main.go
@@ -3,11 +3,12 @@ package main
 
 import "fmt"
 
-func getHumanAge(dogAge int) int {
-	return dogAge * 7
+func getHumanAge(dogAge int) (humanAge int) {
+	humanAge = dogAge * 7
+	return
 }
 
 func main() {
-	dogAge := 5
+	dogAge := 4
 	fmt.Printf("Dog age is %d which in human years is %d.\n",dogAge, getHumanAge(dogAge))
 }
