bf1d3c671c94a6c623acf4e85b6cfc3234f25da8
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-19 11:02:12 +0200

    switch

--- 00200-forLoop/main.go
+++ 00200-forLoop/main.go
@@ -7,9 +7,7 @@ func main() {
 	for i := 0; i < 10; i++ {
 		sum += i
 	}
-
 	word := "dictionary"
-
 	fmt.Printf("The sum is %d\n", sum)
 	fmt.Printf("The length of the word \"%s\" is %d\n", word, len(word))
 }
