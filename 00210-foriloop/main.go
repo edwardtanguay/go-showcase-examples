7ca802d13633d14d4f7152c0a49e893f06ff49ce
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-19 11:12:24 +0200

    dog2

--- 00210-foriloop/main.go
+++ 00210-foriloop/main.go
@@ -4,7 +4,15 @@ package main
 import "fmt"
 
 func main() {
+	separator()
 	for i := 1; i <= 15; i++ {
 		fmt.Printf("when a dog is %d years old, it's human age is %d\n", i, i * 7)
 	}
+
+	separator()
+	for i := 1; i <= 15; i++ {
+		fmt.Printf("Dog: %3d   Human: %4d\n", i, i*7)
+	}
+
+
 }
