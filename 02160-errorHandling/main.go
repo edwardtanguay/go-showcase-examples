713a023fbb1b0d2f4e799057dd59f0ccbbc14ab9
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-19 15:32:01 +0200

    separate

--- 02160-errorHandling/main.go
+++ 02160-errorHandling/main.go
@@ -1,23 +1,36 @@
-
 package main
 
 import "fmt"
 
 type Employee struct {
-	name string
-	department string
+	name        string
+	department  string
 	conferences []string
 }
 
-func (emp *Employee) addSalesConference(name string) {
-	emp.conferences = append(emp.conferences, name)
+func (emp *Employee) addSalesConference(name string) error {
+	if emp.department == "sales" {
+		emp.conferences = append(emp.conferences, name)
+	} else {
+		return fmt.Errorf("%s is not in Sales", emp.name)
+	}
+
+	return nil
 }
 
 func main() {
 	emp1 := Employee{"James Ashton", "sales", []string{"Sales World", "Sales Marketplace"}}
 	emp2 := Employee{"Sven Techton", "it", []string{"JavaScript World", "IT trends"}}
 
-	emp1.addSalesConference("Sales Experts Conference")
+	err := emp1.addSalesConference("Sales Experts Conference")
+	if(err != nil) {
+		fmt.Printf("ERROR: %v", err)
+	}
+
+	err2 := emp2.addSalesConference("Sales Experts Conference")
+	if(err2 != nil) {
+		fmt.Printf("ERROR: %v\n", err2)
+	}
 
 	fmt.Printf("%#v\n", emp1)
 	fmt.Printf("%#v\n", emp2)
