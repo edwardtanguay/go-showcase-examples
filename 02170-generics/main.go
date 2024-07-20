e8f39de6b8f9f2f22541100478376f98f0832b5a
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-20 19:52:20 +0200

    generics

--- 02170-generics/main.go
+++ 02170-generics/main.go
@@ -3,7 +3,7 @@ package main
 
 import "fmt"
 
-func humanAge(dogAge int) int {
+func humanAge [T int|float64](dogAge T) T {
 	return dogAge * 7;
 }
 
@@ -11,6 +11,11 @@ func main() {
 	dogAges := []int{12,4,2,6,1}
 
 	for _, dogAge := range dogAges {
-		fmt.Printf("The dog is %d which in human age is %d.\n", dogAge, humanAge(dogAge))
+		humanAge := humanAge((dogAge))
+		fmt.Printf("The dog is %d (type %T) which in human age is %d (type %T).\n", dogAge, dogAge, humanAge,humanAge)
 	}
+
+	exactDogAge := 4.32
+	exactHumanAge := humanAge((exactDogAge))
+	fmt.Printf("The exact age of the dog is %.2f (type %T)and the exact human age is %.2f (type %T).", exactDogAge, exactDogAge, exactHumanAge, exactHumanAge)
 }
