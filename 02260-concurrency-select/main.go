955262a867476360e8f95fc4ce9ae69826e460c6
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-21 19:28:38 +0200

    some

--- 02260-concurrency-select/main.go
+++ 02260-concurrency-select/main.go
@@ -6,15 +6,19 @@ import (
 )
 
 func makePayment(money chan float64) {
-	amount := <-money
-	fmt.Printf("Received: %.2f\n", amount)
+	select {
+	case amount := <-money:
+		fmt.Printf("Received: %.2f\n", amount)
+	case <-time.After(time.Second * 1):
+		fmt.Print("NOTHING RECEIVED")
+	}
 }
 
 func main() {
 	money := make(chan float64)
 	go makePayment(money)
 
-	money <- 34.99
+	money <- 34.99 // comment out for nothing to be received
 
 	time.Sleep(2 * time.Second)
 }
