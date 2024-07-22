98903d358f266741ca81ed2c214c9f5c4e86f75b
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-22 08:34:08 +0200

    getting file name

--- 00910-cli-dpodiwc/main.go
+++ 00910-cli-dpodiwc/main.go
@@ -3,27 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 )
 
-func writeFile(dirName string, fileName string, content string) {
-
-	filePath := filepath.Join(dirName, fileName)
-
-	file, err := os.Create(filePath)
-
-	if err != nil {
-		fmt.Printf("Error creating file: %v\n", err)
-		return
-	}
-	defer file.Close()
-
-	_, err = file.WriteString(content)
-	if err != nil {
-		fmt.Printf("Error writing to file: %v\n", err)
-		return
-	}
-}
+// build command: go build -o dpodiwc.exe main.go
 
 func main() {
 	if len(os.Args) < 2 {
@@ -33,50 +15,11 @@ func main() {
 		fmt.Println("Example: dpodiwc skills.dpod.txt")
 		fmt.Println("Assumes file is in: C:\\edward\\datapod2023\\datapod-for-vite-react-core\\currentImport")
 		return
-	}
-
-	var dirName = os.Args[1]
-
-	err := os.MkdirAll(dirName, 0755)
-	if err != nil {
-		fmt.Printf("Error creating directory: %v\n", err)
-		return
-	}
-
-	writeFile(dirName, "main.go", `
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("nnn")
-}
-`)
-
-	writeFile(dirName, "tools.go", `
-package main
-
-import (
-	"fmt"
-	"strings"
-)
-
-func separator(title ...string) {
-	width := 50
-	preWidth := 3
-	character := "="
-	if len(title) == 0 || title[0] == "" {
-		fmt.Println(strings.Repeat(character, width))
 	} else {
-		fmt.Printf("%s %s %s\n", strings.Repeat(character, preWidth), strings.ToUpper(title[0]), strings.Repeat(character, width - len(title[0]) - preWidth - 2))
-	}
-}
 
-func checkError(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-`)
+		var fileName = os.Args[1]
 
+		fmt.Printf("fileName = %s\n", fileName)
+
+	}
 }
