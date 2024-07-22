8663d6d703f1110b2e2616fb514812caa754b28d
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-23 00:51:07 +0200

    template finished

--- 02290-web-templates/main.go
+++ 02290-web-templates/main.go
@@ -3,23 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
+type Data struct {
+	SiteTitle string
+	DaysToLive int
+}
+
 func HandleMainRoute (w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, `
+	data := Data{"The Info Site", 7}
+	t,_ := template.New("page").Parse(`
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Info Site</title>
+	<title>{{.SiteTitle}}</title>
 </head>
 <body>
 	<h1>Info Site</h1>
-	<p>Welcome to this site.</p>
+	<p>Welcome to this site, it will be live in {{.DaysToLive}} days.</p>
 </body>
 </html>
 	`)
+	t.Execute(w, data)
 }
 
 func main() {
