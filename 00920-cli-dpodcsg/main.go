bae9bee6e3b255f2b83fe32a1224b659f07b348c
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-22 13:10:15 +0200

    colors

--- 00920-cli-dpodcsg/main.go
+++ 00920-cli-dpodcsg/main.go
@@ -3,18 +3,29 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/fatih/color"
 )
 
 // build command: go build -o dpodcsg.exe main.go
 
+const toScanPathAndFileName = "C:\\edward\\datapod2023\\datapod-for-vite-react-core\\currentImport\\skills.dpod.txt"
+const graphicsFromPath = "C:\\WORK\\import"
+const graphicsToPath = "C:\\edward\\datapod2023\\tanguay-eu\\public\\images\\outline"
+
 func main() {
+	blue := color.New(color.FgBlue).SprintFunc()
+	green := color.New(color.FgGreen).SprintFunc()
+	yellow := color.New(color.FgYellow).SprintFunc()
+	red := color.New(color.FgRed).SprintFunc()
+
 	if len(os.Args) < 2 {
 		fmt.Println("Datapod: Copy Skill Graphics")
-		fmt.Println("1. runs through the file C:\\edward\\datapod2023\\datapod-for-vite-react-core\\currentImport\\skills.dpod.txt")
+		fmt.Printf("1. runs through the file: %s\n", yellow(toScanPathAndFileName))
 		fmt.Println("2. finds all lines ending with ##<idCode>")
-		fmt.Println("3. copies <idCode>.png from C:\\WORK\\import to C:\\edward\\datapod2023\\tanguay-eu\\public\\images\\outline")
-		fmt.Println("Usage: dpodcsg = get this instructions")
-		fmt.Println("Usage: dpodcsg copy = copy the files")
+		fmt.Printf("3. copies <idCode>.png from %s to %s\n", blue(graphicsFromPath), green(graphicsToPath))
+		fmt.Printf("Usage: %s = get this instructions\n", red("dpodcsg"))
+		fmt.Printf("Usage: %s = copy the files\n", red("dpodcsg copy"))
 		return
 	} else {
 
@@ -23,7 +34,7 @@ func main() {
 		if command != "copy" {
 			fmt.Printf("ERROR: the only valid command is \"copy\"")
 		} else {
-			fmt.Printf("copying...")	
+			fmt.Printf("copying...")
 		}
 
 	}
