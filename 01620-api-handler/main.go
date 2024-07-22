c676acb07ead31fead30cb14df4f555fa260f5c3
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-22 23:20:38 +0200

    handler half done

---
+++ 01620-api-handler/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+
+func main() {
+
+	port := 2346
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Info API</title>
+</head>
+<body>
+		<h1>Info API</h1>
+		<p>frameworks = <a href="http://localhost:%d/frameworks">http://localhost:%d/frameworks</a></p>
+</body>
+</html>
+		`, port, port)
+	})
+
+	fmt.Printf("listening at http://localhost:%d", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
