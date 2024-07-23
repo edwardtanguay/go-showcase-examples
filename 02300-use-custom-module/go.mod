793d7a1bb952a9e109da536d27d343289bb013af
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-23 16:26:13 +0200

    gotools

---
+++ 02300-use-custom-module/go.mod
@@ -0,0 +1,5 @@
+module tanguay.info/02300-use-custom-module
+
+go 1.22.1
+
+require github.com/edwardtanguay/gotools v1.3.0
--- 02300-use-custom-module/main.go
+++ 02300-use-custom-module/main.go
@@ -1,12 +1,20 @@
-
 package main
 
-import "fmt"
-import "github.com/edwardtanguay/gotools"
+import (
+	"fmt"
+
+	"github.com/edwardtanguay/gotools"
+)
 
 func main() {
-	lines, _ := files.GetLinesFromFile("main.go")
+	lines, _ := gotools.GetLinesFromFile("main.go")
 	for index,line := range lines {
 		fmt.Printf("%02d | %s\n", index+1, line)
 	}
+
+	for i := 1; i <= 4; i++ {
+		fmt.Printf("Status: %s\n", gotools.SmartPlural(i, "card", ""))
+		fmt.Printf("Status: %s\n", gotools.SmartPlural(i, "bus", "buses"))
+	}
+
 }
