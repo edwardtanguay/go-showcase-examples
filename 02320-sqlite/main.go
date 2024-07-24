0ddba0b1f9051cb6681ee082ba3a0f00fcd21b4f
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-24 18:06:35 +0200

    clean

--- 02320-sqlite/main.go
+++ 02320-sqlite/main.go
@@ -5,15 +5,9 @@ import (
 )
 
 func main() {
-	// employees, err := models.GetEmployees()
 	a := models.App{}
 	a.Port = 9003
 	a.Initialize()
 	a.Run()
 	defer a.DB.Close()
-	// if err != nil {
-	// 	fmt.Printf("Cannot get employees: %v\n", err)
-	// } else {
-	// 	fmt.Printf("There are %d employees:\n", len(employees))
-	// }
 }
