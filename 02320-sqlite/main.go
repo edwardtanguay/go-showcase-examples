ce3e0620e3bf4eba8760f2378694942763171433
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-24 11:19:21 +0200

    sqlite

---
+++ 02320-sqlite/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Employee struct {
+	FirstName string
+	LastName  string
+}
+
+func main() {
+	db, err := sql.Open("sqlite3", "./data/northwind.sqlite")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	rows, err := db.Query("SELECT FirstName, LastName FROM Employees")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var e Employee
+
+		rows.Scan(&e.FirstName, &e.LastName)
+		fmt.Printf("EMPLOYEE: %s, %s", e.LastName, e.FirstName)
+	}
+
+}
