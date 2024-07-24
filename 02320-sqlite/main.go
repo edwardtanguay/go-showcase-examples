74d3e275552ecaf7c1d554b30d2a802abcd8f3db
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-24 17:19:14 +0200

    api starts

--- 02320-sqlite/main.go
+++ 02320-sqlite/main.go
@@ -1,19 +1,21 @@
 package main
 
 import (
-	"fmt"
-
 	"tanguay.info/02320-sqlite/models"
 )
 
 func main() {
-	employees, err := models.GetEmployees()
-	if err != nil {
-		fmt.Printf("Cannot get employees: %v\n", err)
-	} else {
-		fmt.Printf("There are %d employees:\n", len(employees))
-		for i, emp := range employees {
-			fmt.Printf("%d. %s\n", i+1, emp.FirstName+" "+emp.LastName)
-		}
-	}
+	// employees, err := models.GetEmployees()
+	a := models.App{}
+	a.Port = 9003
+	a.Initialize()
+	a.Run()
+	// if err != nil {
+	// 	fmt.Printf("Cannot get employees: %v\n", err)
+	// } else {
+	// 	fmt.Printf("There are %d employees:\n", len(employees))
+	// 	for i, emp := range employees {
+	// 		fmt.Printf("%d. %s\n", i+1, emp.FirstName+" "+emp.LastName)
+	// 	}
+	// }
 }
--- 02320-sqlite/models/tools.go
+++ 02320-sqlite/models/tools.go
@@ -3,30 +3,52 @@ package models
 import (
 	"database/sql"
 	"fmt"
+	"log"
+	"net/http"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func GetEmployees() ([]Employee, error) {
-	db, err := sql.Open("sqlite3", "./data/northwind.sqlite")
-	if err != nil {
-		return nil, fmt.Errorf("error opening database: %v", err)
-	}
-	defer db.Close()
-
-	rows, err := db.Query("SELECT FirstName, LastName FROM Employees")
-	if err != nil {
-		return nil, fmt.Errorf("error querying database: %v", err)
-	}
-	defer rows.Close()
-
-	var employees []Employee
-	for rows.Next() {
-		var e Employee
-		rows.Scan(&e.FirstName, &e.LastName)
-		employee := Employee{FirstName: e.FirstName, LastName: e.LastName}
-		employees = append(employees, employee)
-	}
-
-	return employees, nil
+type App struct {
+	DB *sql.DB
+	Port int
 }
+
+func (a *App) Initialize() error {
+	var err error
+	a.DB, err = sql.Open("sqlite3", "./data/northwind.sqlite")
+	if err != nil {
+		return fmt.Errorf("error opening database: %v", err)
+	}
+	defer a.DB.Close()
+	return nil
+}
+
+func handleHomeRoute(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the API.")
+}
+
+func (a *App) Run() {
+	http.HandleFunc("/", handleHomeRoute)
+	fmt.Printf("API running at http://localhost:%d\n", a.Port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", a.Port), nil))
+}
+
+// func GetEmployees() ([]Employee, error) {
+
+// 	rows, err := db.Query("SELECT FirstName, LastName FROM Employees")
+// 	if err != nil {
+// 		return nil, fmt.Errorf("error querying database: %v", err)
+// 	}
+// 	defer rows.Close()
+
+// 	var employees []Employee
+// 	for rows.Next() {
+// 		var e Employee
+// 		rows.Scan(&e.FirstName, &e.LastName)
+// 		employee := Employee{FirstName: e.FirstName, LastName: e.LastName}
+// 		employees = append(employees, employee)
+// 	}
+
+// 	return employees, nil
+// }
