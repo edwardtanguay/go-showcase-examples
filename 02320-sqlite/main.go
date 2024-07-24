5ec1d443790f5ae4ba05d6c6b21a59ff6952ac02
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-24 16:49:32 +0200

    error throwing

--- 02320-sqlite/main.go
+++ 02320-sqlite/main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"fmt"
+
 	"tanguay.info/02320-sqlite/models"
 )
 
 func main() {
-	employees := models.GetEmployees()
-	fmt.Printf("There are %d employees:\n", len(employees))
-	for i,emp := range employees {
-		fmt.Printf("%d. %s\n", i, emp.FirstName + " " + emp.LastName)
+	employees, err := models.GetEmployees()
+	if err != nil {
+		fmt.Printf("Cannot get employees: %v\n", err)
+	} else {
+		fmt.Printf("There are %d employees:\n", len(employees))
+		for i, emp := range employees {
+			fmt.Printf("%d. %s\n", i+1, emp.FirstName+" "+emp.LastName)
+		}
 	}
 }
--- 02320-sqlite/models/tools.go
+++ 02320-sqlite/models/tools.go
@@ -1,25 +1,26 @@
-package models 
+package models
 
 import (
 	"database/sql"
-	"log"
+	"fmt"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func GetEmployees() (employees []Employee) {
+func GetEmployees() ([]Employee, error) {
 	db, err := sql.Open("sqlite3", "./data/northwind.sqlite")
 	if err != nil {
-		log.Fatal(err.Error())
+		return nil, fmt.Errorf("error opening database: %v", err)
 	}
 	defer db.Close()
 
 	rows, err := db.Query("SELECT FirstName, LastName FROM Employees")
 	if err != nil {
-		log.Fatal(err.Error())
+		return nil, fmt.Errorf("error querying database: %v", err)
 	}
 	defer rows.Close()
 
+	var employees []Employee
 	for rows.Next() {
 		var e Employee
 		rows.Scan(&e.FirstName, &e.LastName)
@@ -27,5 +28,5 @@ func GetEmployees() (employees []Employee) {
 		employees = append(employees, employee)
 	}
 
-	return
+	return employees, nil
 }
