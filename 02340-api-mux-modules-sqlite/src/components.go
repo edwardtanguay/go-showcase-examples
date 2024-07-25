d4774b667faaff57e03ae26fb6eada57b24e4457
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-25 08:52:06 +0200

    background

--- 02340-api-mux-modules-sqlite/src/components.go
+++ 02340-api-mux-modules-sqlite/src/components.go
@@ -8,9 +8,9 @@ import (
 func Header() string {
 	return `
 <h1>Northwind Site</h1>
-<hr>
-<a href="/">Home</a> | <a href="/employees">Employees</a>
-<hr>
+<nav>
+<a href="/">Home</a><a href="/employees">Employees</a>
+</nav>
 	`
 }
 
