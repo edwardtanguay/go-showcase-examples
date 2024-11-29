58b5b0f56e9892da3937c3d05c7320e2a6705367
Edward Tanguay (windows) <edward@tanguay.info>
2024-11-29 13:50:23 +0100

    main works

--- 01630-api-post-route/main.go
+++ 01630-api-post-route/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strconv"
 )
 
 type Flashcard struct {
-	Front string `json: "front"`
-	Back  string `json: "back"`
+	Id    string `json:"id"`
+	Front string `json:"front"`
+	Back  string `json:"back"`
 }
 
 func handlePostCreateFlashcard(w http.ResponseWriter, r *http.Request) {
@@ -18,11 +20,20 @@ func handlePostCreateFlashcard(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	var flashcard Flashcard
+	err := json.NewDecoder(r.Body).Decode(&flashcard)
+	if err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+	}
+	flashcard.Id = "nnn"
+	
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(flashcard)
 }
 
 func main() {
 	port := 3988
 	http.HandleFunc("/create-flashcard", handlePostCreateFlashcard)
-	fmt.Printf("Server running at http://localhost:%s", strconv.Itoa(port))
-	http.ListenAndServe(":" + strconv.Itoa(port), nil)
+	fmt.Printf("Server running at http://localhost:%s\n", strconv.Itoa(port))
+	http.ListenAndServe(":"+strconv.Itoa(port), nil)
 }
