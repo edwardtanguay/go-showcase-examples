b8428504f913231e2a8cd943e38f687084956164
Edward Tanguay (windows) <edward@tanguay.info>
2024-12-08 14:50:46 +0100

    middleware

--- 02380-basic-middleware/main.go
+++ 02380-basic-middleware/main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strconv"
 )
 
 type User struct {
-	login    string
-	password string
+	Login    string `json:"login"`
+	Password string `json:"password`
 }
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
@@ -16,9 +17,28 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Welcome!"))
 }
 
+func authentication(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var newUser User
+
+		err := json.NewDecoder(r.Body).Decode(&newUser)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		if newUser.Login != "hans" || newUser.Password != "123" {
+			http.Error(w, "no access", http.StatusUnauthorized)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	port := 7283
-	http.HandleFunc("/login", loginHandler)
+	mux := http.NewServeMux()
+	middleware := authentication(http.HandlerFunc(loginHandler))
+	mux.Handle("/login", middleware)
 	fmt.Printf("listening at: http://localhost:%d", port)
-	http.ListenAndServe(":"+strconv.Itoa(port), nil)
+	http.ListenAndServe(":"+strconv.Itoa(port), mux)
 }
